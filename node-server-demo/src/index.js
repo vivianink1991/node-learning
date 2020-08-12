507800a9f6064e8fab4b172b889a462c0f3410fd
vivianink1991 <binggan26@126.com>
2020-08-12 23:22:41 +0800

    first commit

---
+++ node-server-demo/src/index.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-12 22:34:08
+ * @LastEditTime: 2020-08-12 22:54:12
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /node-learning/node-server-demo/src/index.js
+ */
+const router = require('./router')
+const server = require('./server')
+
+const requestHandlers = require("./requestHandler")
+const handle = {
+    '/': requestHandlers.start,
+    '/start': requestHandlers.start,
+    '/upload': requestHandlers.upload
+}
+
+// 解耦requestHandlers和route
+server.start(router.route, handle)
---
+++ node-server-demo/src/server.js
@@ -0,0 +1,36 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-12 22:34:02
+ * @LastEditTime: 2020-08-12 23:19:39
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /node-learning/node-server-demo/src/server.js
+ */
+const http = require('http')
+const url = require('url')
+
+const start = function(route, handle) {
+    function onRequest(request, response){
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for "+ pathname +" received.");
+        
+        let postData = ''
+        request.setEncoding("utf8");
+
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '" + postDataChunk +"'.");
+        });
+
+        request.addListener("end", function() {
+            route(handle, pathname, response, postData);
+        });
+    }
+    const server = http.createServer(onRequest)
+    server.listen(9000)
+    console.log('Server starts listen at 9000')
+}
+
+module.exports = {
+    start
+}
